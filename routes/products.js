e47266b1a0be09b968b431553c853782a20a300e
Kenn Mutai <mutaikenn@gmail.com>
2020-03-18 19:41:22 +0200

    Mongo database included

--- routes/products.js
+++ routes/products.js
@@ -1,37 +1,87 @@
 const router = require("express").Router();
+const Product = require("../model/product");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET request to products page"
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      if (docs.length >= 0) {
+        res.status(200).json(docs);
+      } else {
+        res.status(404).json({ message: "No entries found " });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 router.post("/", (req, res, next) => {
-  res.status(201).json({
-    message: "Handling POST request to products page"
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
   });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handling POST request to products page",
+        createdProduct: product
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  if (id === "123") {
-    res.status(200).json({
-      message: "You include an Id",
-      id: id
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ message: "No valid entry found for the id" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(200).json({
-      message: "You passed an Id"
-    });
-  }
 });
 router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "You updated products page"
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "You deleted products"
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const PORT = process.env.PORT || 3000;
 
 // Import Routes
 const productRoutes = require("./routes/products");
 const orderRoutes = require("./routes/orders");
 
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+  process.env.MONGO_DB,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to DB!")
+);
+
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handling CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow_Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT",
+      "POST",
+      "PATCH",
+      "DELETE",
+      "GET"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Routes Middleware
 app.use("/products", productRoutes);
