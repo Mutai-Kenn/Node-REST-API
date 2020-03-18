ad1ae95624bf0fbbbe9e6039cd204e84a63af229
Kenn Mutai <mutaikenn@gmail.com>
2020-03-18 13:40:41 +0200

    Handling route errors

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const PORT = process.env.PORT || 3000;
 
 // Import Routes
 const productRoutes = require("./routes/products");
 const orderRoutes = require("./routes/orders");
 
+app.use(morgan("dev"));
+
 // Routes Middleware
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+// Handling errors that passes thru' the routes
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+// Handling all the errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
