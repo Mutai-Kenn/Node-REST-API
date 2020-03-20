1b84ffa2b5c8b60968c8fcbb6ecff3320d6b34a5
Kenn Mutai <mutaikenn@gmail.com>
2020-03-20 14:25:19 +0200

    Product and Orders routes/ models included

--- routes/products.js
+++ routes/products.js
@@ -4,14 +4,24 @@ const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("-_v")
     .exec()
     .then(docs => {
-      console.log(docs);
-      if (docs.length >= 0) {
-        res.status(200).json(docs);
-      } else {
-        res.status(404).json({ message: "No entries found " });
-      }
+      const response = {
+        count: docs.length,
+        product: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: req.get("host") + "/products/" + doc._id
+            }
+          };
+        })
+      };
+      res.status(200).json(response);
     })
     .catch(err => {
       console.log(err);
@@ -61,10 +71,16 @@ router.patch("/:productId", (req, res, next) => {
   for (const ops of req.body) {
     updateOps[ops.propName] = ops.value;
   }
-  Product.update({ _id: id }, { $set: updateOps })
+  Product.updateOne({ _id: id }, { $set: updateOps })
     .exec()
     .then(result => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product updated",
+        request: {
+          type: "GET",
+          url: req.get("host") + "/products/" + id
+        }
+      });
     })
     .catch(err => {
       console.log(err);
@@ -73,10 +89,15 @@ router.patch("/:productId", (req, res, next) => {
 });
 router.delete("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  Product.remove({ _id: id })
+  Product.deleteOne({ _id: id })
     .exec()
     .then(result => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product deleted",
+        type: "POST",
+        url: req.get("host") + "/products/",
+        body: { name: "String", price: "Number" }
+      });
     })
     .catch(err => {
       console.log(err);
