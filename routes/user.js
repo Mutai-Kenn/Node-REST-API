72814b221ffc277debf96b9ef3a9eda16ac5afe1
Kenn Mutai <mutaikenn@gmail.com>
2020-03-23 10:47:08 +0200

    Login user included

--- routes/user.js
+++ routes/user.js
@@ -43,6 +43,38 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user < 1) {
+        return res.status(401).json({
+          message: "Auth Failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth Failed"
+          });
+        }
+        if (result) {
+          return res.status(200).json({
+            message: "Auth successful"
+          });
+        }
+        res.status(401).json({
+          message: "Auth Failed"
+        });
+      });
+    })
+
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
 router.delete("/:userId", (req, res, next) => {
   User.remove({ _id: req.params.userId })
     .exec()
